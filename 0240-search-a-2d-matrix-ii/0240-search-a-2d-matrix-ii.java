387d41201ab2755f064e6b20e51e6534c14dcc3c
Waseeyur Rahman <waseeyurrahman@gmail.com>
2026-07-14 12:08:08 +0530

    Time: 4 ms (23.39%), Space: 48.1 MB (64.58%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    static boolean search(int[] arr ,int  target){
+        int low = 0, high = arr.length-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == target) return true;
+            else if(arr[mid] < target){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0 ;i < matrix.length; i++) {
+            if(search(matrix[i], target)){
+                return true;
+            } 
+        }
+        return false;
+    }
+}
